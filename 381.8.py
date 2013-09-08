ec4fd28188f99bbeb86427e938cea5ccb824021b
michellejaeg <michelles82@gmail.com>
2013-09-08 16:40:05 -0700

    changed solution and added tests

--- 381.8.py
+++ 381.8.py
@@ -2,12 +2,43 @@
 
 # Write and test a function removeDuplicates(somelist) that removes duplicate values from a list
 
-def removeDuplicates(somelist):
-    for item in somelist:
-        total=somelist.count(item)
-        if total>1:
-            for i in range(total-1):
-                somelist.remove(item)
-    return somelist
+import unittest
 
-print (removeDuplicates([1,2,4,4,3]))
+def removeDuplicates(somelist):
+    newlist = []
+    for num in somelist:
+        if num not in newlist:
+            newlist.append(num)
+    return newlist
+
+class MyTest(unittest.TestCase):
+
+    def test_Middle(self):
+        # Multiples in middle
+        self.assertEqual(removeDuplicates([1,2,2,3]), [1,2,3])
+
+    def test_Empty(self):
+        # empty list
+        self.assertEqual(removeDuplicates([]), [])
+
+    def test_One(self):
+        # One item in list
+        self.assertEqual(removeDuplicates([1]), [1])
+
+    def test_no_Multiples(self):
+        # No multiples
+        self.assertEqual(removeDuplicates([1,2,3,4]), [1,2,3,4])
+
+    def test_Only(self):
+        # Only multiples
+        self.assertEqual(removeDuplicates([2,2,2,2]), [2])
+
+    def test_Beginning(self):
+        # Multiples at beginning
+        self.assertEqual(removeDuplicates([5,5,7,8]), [5,7,8])
+
+    def test_End(self):
+        # Multiples at end
+        self.assertEqual(removeDuplicates([1,2,6,6]), [1,2,6])
+
+
