e34f2ee4580e21678f6a21776a66633f536a13c6
MichelleJaeg <michelles82@gmail.com>
2013-11-18 15:32:54 -0800

    Added spaces to make code more readable

--- 383.15.py
+++ 383.15.py
@@ -12,25 +12,25 @@ from random import shuffle
 class Deck:
 
     def __init__(self):
-        deck=[]
-        self.deck=deck
+        deck = []
+        self.deck = deck
         for i in range(4):
-            if i==0:
+            if i == 0:
                 suit = 'Clubs'
-            elif i==1:
+            elif i == 1:
                 suit = 'Diamonds'
-            elif i==2:
+            elif i == 2:
                 suit = 'Hearts'
-            elif i==3:
+            elif i == 3:
                 suit = 'Spades'
             for j in range(2,15):
-                if j==11:
+                if j == 11:
                     rank = 'Jack'
-                elif j==12:
+                elif j == 12:
                     rank = 'Queen'
-                elif j==13:
+                elif j == 13:
                     rank = 'King'
-                elif j==14:
+                elif j == 14:
                     rank = "Ace"
                 else:
                     rank = str(j)
@@ -41,19 +41,19 @@ class Deck:
         shuffle(self.deck)
 
     def dealCard(self):
-        card=self.deck[0]
+        card = self.deck[0]
         self.deck.remove(card)
         return card
 
     def cardsLeft(self):
-        counter=0
+        counter = 0
         for card in self.deck:
-            counter+=1
+            counter += 1
         return counter
 
 
 def main ():
-    deck=Deck()
+    deck = Deck()
     deck.shuffle()
     n = eval(input("How many cards would you like the program to deal? "))
     print ()
