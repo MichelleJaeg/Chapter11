967f26253275b33577d9b6a21b8005e483c4f187
MichelleJaeg <michelles82@gmail.com>
2013-11-18 15:15:28 -0800

    Added spaces to make code more readable

--- 381.10.py
+++ 381.10.py
@@ -12,26 +12,26 @@ def main ():
     n = eval(input("Please enter a number. "))
 
     #create lists
-    listOfNumbers=[]
-    primes=[]
-    for i in range(2, n+1):
+    listOfNumbers = []
+    primes = []
+    for i in range(2, n + 1):
         listOfNumbers.append(i)
 
     # while the list is not empty
-    while listOfNumbers!=[]:
-        number=listOfNumbers[0]
+    while listOfNumbers != []:
+        number = listOfNumbers[0]
         # Add the number to the primes list and remove from original list
         primes.append(number)
         listOfNumbers.remove(number)
         # Find numbers that are left that divide evenly by the number
-        i=0
-        while i<len(listOfNumbers):
-            num2=listOfNumbers[i]
-            if num2%number==0 and num2!=number:
+        i = 0
+        while i < len(listOfNumbers):
+            num2 = listOfNumbers[i]
+            if num2 % number == 0 and num2 != number:
                 # Remove number
                 listOfNumbers.remove(num2)
-                i-=1
-            i+=1
+                i -= 1
+            i += 1
 
 
 
