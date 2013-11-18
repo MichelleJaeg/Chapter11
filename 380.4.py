5be6b2137d052015a48d23b7257c0fb3dc8aee12
MichelleJaeg <michelles82@gmail.com>
2013-11-18 15:11:40 -0800

    Update 380.4.py
    Added spaces to make code more readable.

--- 380.4.py
+++ 380.4.py
@@ -24,39 +24,39 @@ def writeStudents(students, filename):
     for s in students:
         print ("{0}\t{1}\t{2}".
         format(s.getName(), s.getHours(), s.getQPoints()),
-            file=outfile)
+            file = outfile)
     outfile.close()
 
 def main ():
 
     #Create window and Entry objects
-    win=GraphWin('Data Sorting',400,200)
-    inputText=Text(Point(95,40),"Enter the name of the data file")
+    win = GraphWin('Data Sorting',400,200)
+    inputText = Text(Point(95,40),"Enter the name of the data file")
     inputText.draw(win)
-    inputFileName=Entry(Point(280,40),30)
+    inputFileName = Entry(Point(280,40),30)
     inputFileName.draw(win)
-    outputText=Text(Point(95,70),"Enter the name of the output file")
+    outputText = Text(Point(95,70),"Enter the name of the output file")
     outputText.draw(win)
-    outputFileName=Entry(Point(280,70),30)
+    outputFileName = Entry(Point(280,70),30)
     outputFileName.draw(win)
 
     # Introduction
-    messageText=Text(Point(200,10),"This program sorts student data from one file and outputs it to another file.")
+    messageText = Text(Point(200,10),"This program sorts student data from one file and outputs it to another file.")
     messageText.draw(win)
 
     # Create buttons
-    GPAsort=Button(win, Point(60,130),80,20,"Sort by GPA")
-    Namesort=Button(win,Point(180, 130),80,20,"Sort by Name")
-    Creditssort=Button(win,Point(300,130),80,20,"Sort by Credits")
-    ascendingButton=Button(win, Point(120, 160),100, 20, "Ascending Order")
-    descendingButton=Button(win, Point(260,160),100,20, "Descending Order")
-    quitButton=Button(win,Point(370,180),50,20, "Quit")
-    enterButton=Button(win,Point(300,100),120,20, "Submit file info")
-    sortButton=Button(win,Point(100,100),80,20, "Sort Data")
+    GPAsort = Button(win, Point(60,130),80,20,"Sort by GPA")
+    Namesort = Button(win,Point(180, 130),80,20,"Sort by Name")
+    Creditssort = Button(win,Point(300,130),80,20,"Sort by Credits")
+    ascendingButton = Button(win, Point(120, 160),100, 20, "Ascending Order")
+    descendingButton = Button(win, Point(260,160),100,20, "Descending Order")
+    quitButton = Button(win,Point(370,180),50,20, "Quit")
+    enterButton = Button(win,Point(300,100),120,20, "Submit file info")
+    sortButton = Button(win,Point(100,100),80,20, "Sort Data")
 
     # while user has not chosen to quit the program
     enterButton.activate()
-    p=win.getMouse()
+    p = win.getMouse()
     while not quitButton.clicked(p):
 
     # obtain data to sort and file to output to
@@ -71,13 +71,13 @@ def main ():
         GPAsort.activate()
         Namesort.activate()
         Creditssort.activate()
-        p=win.getMouse()
+        p = win.getMouse()
         if GPAsort.clicked(p):
-            key=Student.gpa
+            key = Student.gpa
         elif Namesort.clicked(p):
-            key=Student.getName
+            key = Student.getName
         else:
-            key=Student.getHours
+            key = Student.getHours
         GPAsort.deactivate()
         Namesort.deactivate()
         Creditssort.deactivate()
@@ -86,11 +86,11 @@ def main ():
         messageText.setText("Please click a button to choose to sort in ascending or descending order.")
         ascendingButton.activate()
         descendingButton.activate()
-        p=win.getMouse()
+        p = win.getMouse()
         if descendingButton.clicked(p):
-            reverse=True
+            reverse = True
         else:
-            reverse=False
+            reverse = False
         ascendingButton.deactivate()
         descendingButton.deactivate()
 
@@ -98,7 +98,7 @@ def main ():
         messageText.setText("Click the sort button to sort the data.")
         sortButton.activate()
         win.getMouse()
-        data.sort(key=key, reverse=reverse)
+        data.sort(key = key, reverse = reverse)
         writeStudents(data, filename2)
         sortButton.deactivate()
 
