70745e75ea56b488e6ca63b136d8c1720bce76e0
MichelleJaeg <michelles82@gmail.com>
2013-11-18 15:18:20 -0800

    Added spaces to make code more readable

--- 381.7.py
+++ 381.7.py
@@ -3,9 +3,9 @@
 # Write and test a function innerProd(x,y) that computes the inner product of two (same length) lists.
 
 def innerProd(x,y):
-    sum=0
+    sum = 0
     for i in range(len(x)):
-        sum+=x[i]* y[i]
+        sum += x[i] * y[i]
     return sum
 
 print (innerProd([1,2],[3,5]))
