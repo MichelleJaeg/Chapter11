c79f9ca6638d29eb16bfd71b47d037566e81c14e
MichelleJaeg <michelles82@gmail.com>
2013-11-18 15:46:13 -0800

    Added spaces to make code more readable

--- 384.18.py
+++ 384.18.py
@@ -9,23 +9,23 @@ import random
 def main (numtrials):
 
     #Get input
-    n=eval(input("\nHow many squares long would you like the sidewalk to be? "))
+    n = eval(input("\nHow many squares long would you like the sidewalk to be? "))
 
     #Simulation
     totalSquaresCount=[]
     for i in range(numtrials):
-        location=n/2
+        location = n / 2
         totalSquaresCount.append(location)
-        while location>0 and location<n:
-            step=random.choice([-1,1])
-            location+=step
+        while location > 0 and location < n:
+            step = random.choice([ - 1, 1])
+            location += step
             totalSquaresCount.append(location)
     for i in range(n):
-        total=totalSquaresCount.count(i)
-        print (" Square number " + str(i+1)+  " was landed on " + str(total) + " times.")
+        total = totalSquaresCount.count(i)
+        print (" Square number " + str(i + 1) +  " was landed on " + str(total) + " times.")
 
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main (100)
