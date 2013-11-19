4c2324418acd154d23daa980f3f87d1f4152707c
MichelleJaeg <michelles82@gmail.com>
2013-11-18 16:01:07 -0800

    Added spaces to make code more readable

--- gpasort.py
+++ gpasort.py
@@ -8,7 +8,7 @@
 #  ascending or descending order. "
 
 # gpasort.py
-#    A program to sort student information into GPA order
+# A program to sort student information into GPA order
 
 from gpa import Student, makeStudent
 
@@ -25,7 +25,7 @@ def writeStudents(students, filename):
     for s in students:
         print ("{0}\t{1}\t{2}".
                  format(s.getName(), s.getHours(), s.getQPoints()),
-            file=outfile)
+            file = outfile)
     outfile.close()
 
 def main ():
@@ -33,20 +33,20 @@ def main ():
     filename = input ("Enter the name of the data file: \n")
     data = readStudents (filename)
     direction = input("Would you like the data sorted in ascending or descending order?\n")
-    if direction=="descending":
-        reverse=True
+    if direction == "descending":
+        reverse = True
     else:
-        reverse=False
-    sortType=input("Would you like the data sorted by GPA, name, or credits?\n")
-    if sortType=="GPA":
-        data.sort(key=Student.gpa, reverse=reverse)
-    elif sortType=="name":
-        data.sort(key=Student.getName, reverse=reverse)
+        reverse = False
+    sortType = input("Would you like the data sorted by GPA, name, or credits?\n")
+    if sortType == "GPA":
+        data.sort(key = Student.gpa, reverse=reverse)
+    elif sortType == "name":
+        data.sort(key = Student.getName, reverse=reverse)
     else:
-        data.sort(key=Student.getHours, reverse=reverse)
+        data.sort(key = Student.getHours, reverse = reverse)
     filename = input("Enter a name for the output file: \n")
     writeStudents(data, filename)
     print ("The data has been written to", filename)
 
-if __name__== '__main__':
+if __name__ == '__main__':
     main ()
